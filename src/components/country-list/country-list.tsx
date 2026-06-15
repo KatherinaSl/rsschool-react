78e39c08465abb2455cc29bd586f29446a2c082e
KatherinaSl <106989490+KatherinaSl@users.noreply.github.com>
2026-06-15 15:16:22 +0500

    feat: add proper key props

--- src/components/country-list/country-list.tsx
+++ src/components/country-list/country-list.tsx
@@ -63,9 +63,9 @@ export const CountryList = ({
 
   return (
     <div className={styles.countryList}>
-      {filteredCountries.map((country, index) => (
+      {filteredCountries.map((country) => (
         <CountryCard
-          key={index}
+          key={country.id}
           country={country}
           selectedYear={selectedYear}
           selectedColumns={selectedColumns}
